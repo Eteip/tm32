ac7746aa7a13fd3cbf9cc033af2cee3fd55b8170
‘ezra <digitaljunkie32@gmail.com>
2023-07-05 23:22:37 +0100

    dest commit

---
+++ owlya.js
@@ -0,0 +1,26 @@
+//   tagInputOnBlur(event){
+//     let input = event.target;
+//     const value = event.target.value;
+//     this.handleTagging(value, input)
+
+//   }
+
+//     addTag(event: MatChipInputEvent): void {
+// 		let input = event.input;
+// 		const value = event.value;
+// 		this.handleTagging(value, input)
+
+// 	}
+
+//       removeTag(tag) {
+//     this.tags.delete(tag);
+//   }
+
+
+//   selected(event: MatAutocompleteSelectedEvent): void {
+//     this.tagInput.nativeElement.value = '';
+//     this.tags.add(event.option.viewValue)
+//     this.tagCtrl.setValue(null);
+//   }
+
+  
